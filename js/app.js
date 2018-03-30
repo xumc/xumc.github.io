68a81f87dec2b59b474cb87971179c6ec1edac98
mxu <mxu@freewheel.tv>
2018-03-31 00:27:11 +0800

    add background animation

--- js/app.js
+++ js/app.js
@@ -23,7 +23,7 @@ particlesJS('particles-js',
       }
     },
     "color": {
-      "value": "#0000FF"
+      "value": "#1E90FF"
     },
     "shape": {
       "type": "circle",
@@ -63,7 +63,7 @@ particlesJS('particles-js',
     "line_linked": {
       "enable": true,
       "distance": 150,
-      "color": "#0000FF",
+      "color": "#1E90FF",
       "opacity": 0.4,
       "width": 1
     },
